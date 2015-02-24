f9d35ff51e7e5af913bf2d5f1645a18e26de6ca6
Roberto Lucha <rlucha@gmail.com>
2015-02-24 09:24:59 +0100

    1st

---
+++ README.md
@@ -0,0 +1,2 @@
+# retroChat
+retro... chat!
---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var router = express.Router();
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(1337);
+
+/* ROUTES */
+
+router.get('/', function(req, res) {
+    console.log("root");
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+router.get('/chatroom', function(req, res) {
+    res.sendFile(__dirname + '/chatroom.html');
+});
+
+router.get('/app.css', function(req, res) {
+    res.sendFile(__dirname + '/app.css');
+});
+
+
+app.use('/', router);
